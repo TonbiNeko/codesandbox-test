0c5de066cacbab93dd1716bb8c3269b6a65e1b80
TonbiNeko <68767326+TonbiNeko@users.noreply.github.com>
2021-09-18 16:10:42 +0900

    constの変数宣言について

--- src/index.js
+++ src/index.js
@@ -12,12 +12,21 @@
 // var val1 = "var変数を再宣言";
 // console.log(val1);
 
-let val2 = "let変数";
-console.log(val2);
+// let val2 = "let変数";
+// console.log(val2);
 
-//letは上書きが可能
-val2 = "let変数を上書き";
-console.log(val2);
+// //letは上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
 
-//letは再宣言不可能
-let val2 = "let変数を再宣言";
+// //letは再宣言不可能
+// let val2 = "let変数を再宣言";
+
+const val3 = "const変数";
+console.log(val3);
+
+//const変数は上書き、再宣言不可
+// val3 = "const変数を上書き";
+//val3 is read-only
+
+const val3 = "const変数を再宣言";
