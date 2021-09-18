e46946bdd1aecea976d2f176cc3f9d686544c9f0
TonbiNeko <68767326+TonbiNeko@users.noreply.github.com>
2021-09-18 16:21:09 +0900

    constでobjectを宣言

--- src/index.js
+++ src/index.js
@@ -22,11 +22,21 @@
 // //letは再宣言不可能
 // let val2 = "let変数を再宣言";
 
-const val3 = "const変数";
-console.log(val3);
+// const val3 = "const変数";
+// console.log(val3);
 
-//const変数は上書き、再宣言不可
-// val3 = "const変数を上書き";
-//val3 is read-only
+// //const変数は上書き、再宣言不可
+// // val3 = "const変数を上書き";
+// //val3 is read-only
 
-const val3 = "const変数を再宣言";
+// const val3 = "const変数を再宣言";
+
+//constで定義してもobjectの中身は上書きできる
+//プロパティを変更、追加できる
+const val4 = {
+  name: "J2",
+  age: 33
+};
+val4.name = "object val4のnameを上書き";
+val4.address = "Yokohama";
+console.log(val4);
